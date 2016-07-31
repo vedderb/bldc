956d93c26b3f94c088b448f8bb82e6af10f1906e
Andrew Rossignol <andrew.rossignol@gmail.com>
2016-07-30 23:33:19 -0700

    Adds a new C++ Nunchuk driver.
    - Exposes events through a pure virtual interface.
    - Handles still detection to estimate that the remote is no longer
      in range.

---
+++ drivers/nunchuk.cpp
@@ -0,0 +1,179 @@
+#include "drivers/nunchuk.h"
+
+#include "inttypes.h"
+#include "commands.h"
+
+namespace drivers {
+
+// Maximum time allowed for an i2c transaction.
+constexpr int kNunchukTImeoutMilliseconds(5);
+
+// The address of the nunchuk controller.
+constexpr i2caddr_t kNunchukAddress(0x52);
+
+Nunchuk::Nunchuk(I2CDriver *i2c_driver, NunchukEventHandler *event_handler,
+                 int sample_period_ms, int max_still_count)
+    : i2c_driver_(i2c_driver),
+      event_handler_(event_handler),
+      sample_period_ms_(sample_period_ms),
+      still_count_(0),
+      max_still_count_(max_still_count) {
+}
+
+/*
+ * Obtains the button state from a given byte and a bit position.
+ */
+constexpr ButtonState ParseButtonState(uint8_t byte, uint8_t bit_pos) {
+  return static_cast<ButtonState>(((byte >> bit_pos) & 0x01) ^ 0x01);
+}
+
+void Nunchuk::Start() {
+  while (1) {
+    // Try to initialize until it succeeds. This can happen if the remote is
+    // not connected wirelessly.
+    while (!Initialize()) {
+      chThdSleepMilliseconds(100);
+    }
+  
+    while (1) {
+      // Issue a request for a sample.
+      const uint8_t read_command[1] = { 0x00 };
+      if (!WriteBuffer(read_command, sizeof(read_command))) {
+        break;
+      }
+  
+      // Read the sample into a buffer.
+      uint8_t response[7];
+      if (!ReadBuffer(response, sizeof(response))) {
+        break;
+      }
+  
+      // Parse the buffer into the various parameters of the nunchuk.
+      int8_t joystick_x = (response[0] + 128);
+      int8_t joystick_y = (response[1] + 128);
+      int16_t accel_x = ((response[2] << 2) | ((response[5] >> 2) & 0x03));
+      int16_t accel_y = ((response[3] << 2) | ((response[5] >> 5) & 0x03));
+      int16_t accel_z = ((response[4] << 2) | ((response[5] >> 6) & 0x03));
+      ButtonState button_c = ParseButtonState(response[5], 1);
+      ButtonState button_z = ParseButtonState(response[5], 0);
+  
+      commands_printf("x: %" PRId8 ", y: %" PRId8
+                      ", x: %" PRId16 ", y: %" PRId16 " z: %" PRId16
+                      " c: %" PRIu8 ", z: %" PRIu8,
+                          joystick_x, joystick_y,
+                          accel_x, accel_y, accel_z,
+                          button_c, button_z);
+  
+      // Handle and post events.
+      bool event_handled = HandleAccelSample(accel_x, accel_y, accel_z);
+      event_handled |= HandleButtonCSample(button_c);
+      event_handled |= HandleButtonZSample(button_z);
+      event_handled |= HandleJoystickSample(joystick_x, joystick_y);
+      HandleStillState(event_handled);
+  
+      // Sleep for the sampling interval.
+      chThdSleepMilliseconds(sample_period_ms_);
+    }
+  }
+}
+
+bool Nunchuk::HandleAccelSample(int16_t accel_x, int16_t accel_y,
+                                int16_t accel_z) {
+  if (accel_x != accel_x_ || accel_y != accel_y_ || accel_z != accel_z_) {
+    event_handler_->OnNunchukAccelChange(this, accel_x, accel_y, accel_z);
+    accel_x_ = accel_x;
+    accel_y_ = accel_y;
+    accel_z_ = accel_z;
+    return true;
+  }
+
+  return false;
+}
+
+bool Nunchuk::HandleButtonCSample(ButtonState button_c) {
+  if (button_c != button_c_) {
+    event_handler_->OnNunchukButtonCChange(this, button_c);
+    button_c_ = button_c;
+    return true;
+  }
+
+  return false;
+}
+
+bool Nunchuk::HandleButtonZSample(ButtonState button_z) {
+  if (button_z != button_z_) {
+    event_handler_->OnNunchukButtonZChange(this, button_z);
+    button_z_ = button_z;
+    return true;
+  }
+
+  return false;
+}
+
+bool Nunchuk::HandleJoystickSample(int8_t joystick_x, int8_t joystick_y) {
+  if (joystick_x != joystick_x_ || joystick_y != joystick_y_) {
+    event_handler_->OnNunchukJoystickChange(this, joystick_x, joystick_y);
+    joystick_x_ = joystick_x;
+    joystick_y_ = joystick_y;
+    return true;
+  }
+
+  return false;
+}
+
+void Nunchuk::HandleStillState(bool event_handled) {
+  if (event_handled) {
+    if (still_count_ == max_still_count_) {
+      event_handler_->OnNunchukInMotion(this);
+    }
+
+    still_count_ = 0;
+  } else {
+    if (still_count_ == (max_still_count_ - 1)) {
+      event_handler_->OnNunchukStill(this);
+    }
+
+    still_count_++;
+    if (still_count_ >= max_still_count_) {
+      still_count_ = max_still_count_;
+    }
+  }
+}
+
+bool Nunchuk::WriteBuffer(const uint8_t *buffer, size_t length) {
+  i2cAcquireBus(i2c_driver_);
+  msg_t status = i2cMasterTransmitTimeout(i2c_driver_, kNunchukAddress,
+                                          buffer, length, nullptr, 0,
+                                          MS2ST(kNunchukTImeoutMilliseconds));
+  i2cReleaseBus(i2c_driver_);
+
+  return status == MSG_OK;
+}
+
+bool Nunchuk::ReadBuffer(uint8_t *buffer, size_t length) {
+   i2cAcquireBus(i2c_driver_);
+  msg_t status = i2cMasterReceiveTimeout(i2c_driver_, kNunchukAddress,
+                                         buffer, length,
+                                         MS2ST(kNunchukTImeoutMilliseconds));
+  i2cReleaseBus(i2c_driver_);
+
+  return status == MSG_OK;
+ 
+}
+
+bool Nunchuk::Initialize() {
+  const uint8_t init_command[2][2] = {
+    { 0xF0, 0x55, },
+    { 0xFB, 0x00, },
+  };
+
+  for (size_t i = 0; i < sizeof(init_command) / sizeof(init_command[0]); i++) {
+    if (!WriteBuffer(init_command[i], sizeof(init_command[i]))) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+}  // namespace drivers
---
+++ drivers/nunchuk.h
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2016 Andrew Rossignol andrew.rossignol@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DRIVERS_NUNCHUK_H_
+#define DRIVERS_NUNCHUK_H_
+
+#include <cstdint>
+#include <hal.h>
+
+#include "util/noncopyable.h"
+
+namespace drivers {
+
+class Nunchuk;
+
+/*
+ * The states possible for Nunchuk buttons.
+ */
+enum class ButtonState : uint8_t {
+  Released,
+  Pressed,
+};
+
+/*
+ * An interface to handle Nunchuk events.
+ */
+class NunchukEventHandler {
+ public:
+  ~NunchukEventHandler() {}
+
+  /*
+   * Invoked when the joystick value has changed.
+   */
+  virtual void OnNunchukJoystickChange(Nunchuk *nunchuk, int8_t x, int8_t y) = 0;
+
+  /*
+   * Invoked when a 'C' button state change occurs.
+   */
+  virtual void OnNunchukButtonCChange(Nunchuk *nunchuk, ButtonState state) = 0;
+
+  /*
+   * Invoked when a 'Z' button state change occurs.
+   */
+  virtual void OnNunchukButtonZChange(Nunchuk *nunchuk, ButtonState state) = 0;
+
+  /*
+   * Invoked when the accelerometer value has changed.
+   */
+  virtual void OnNunchukAccelChange(Nunchuk* nunchuk,
+                                    int16_t x, int16_t y, int16_t z) = 0;
+
+  /*
+   * Invoked when the remote transitions from still to in motion. This happens
+   * when an event is published after the maximum number of no-event samples
+   * have been taken.
+   */
+  virtual void OnNunchukInMotion(Nunchuk *nunchuk) {}
+
+  /*
+   * Invoked when no events have been published for a maximum number of
+   * samples. The remote is assumed to be stationary if this happens.
+   */
+  virtual void OnNunchukStill(Nunchuk *nunchuk) {}
+};
+
+/*
+ * A Nintendo Wii Nunchuk driver.
+ */
+class Nunchuk : public util::NonCopyable {
+ public:
+  /*
+   * Construct a Nunchuk driver given an i2c driver, event handler, sampling
+   * period in milliseconds and a maximum still accel event counter. When
+   * max_still_count consequitive accelerometer values are detected, stillness
+   * is asserted. This can be useful for detecting remote connection loss.
+   */
+  Nunchuk(I2CDriver *i2c_driver, NunchukEventHandler *event_handler,
+          int sample_period_ms, int max_stil_count);
+
+  /*
+   * Starts an event loop. Changes in controller state are exposed to a client
+   * through the NunchukEventHandler.
+   */
+  void Start();
+
+ private:
+  // The i2c peripheral used to communicate with the Nunchuk receiver.
+  I2CDriver *i2c_driver_;
+
+  // The event handler to which state changes are passed.
+  NunchukEventHandler *event_handler_;
+
+  // The sampling period of data from the remote.
+  int sample_period_ms_;
+
+  // Stillness detector state.
+  int still_count_;
+  const int max_still_count_;
+
+  // Current state of the joystick for event generation logic.
+  int8_t joystick_x_;
+  int8_t joystick_y_;
+  int16_t accel_x_;
+  int16_t accel_y_;
+  int16_t accel_z_;
+  ButtonState button_c_;
+  ButtonState button_z_;
+
+  /*
+   * Processes an accel sample and posts an event if necessary. Returns true
+   * if an event was generated.
+   */
+  bool HandleAccelSample(int16_t accel_x, int16_t accel_y, int16_t accel_z);
+
+  /*
+   * Processes a C button sample and posts an event if necessary. Returns true
+   * if an event was generated.
+   */
+  bool HandleButtonCSample(ButtonState button_c);
+
+  /*
+   * Processes a Z button sample and posts an event if necessary. Returns true
+   * if an event was generated.
+   */
+  bool HandleButtonZSample(ButtonState button_z);
+
+  /*
+   * Processes a joystick sample and posts an event if necessary. Returns true
+   * if an event was generated.
+   */
+  bool HandleJoystickSample(int8_t joystick_x, int8_t joystick_y);
+
+  /*
+   * Checks for no motion/any motion and posts an event as necessary.
+   */
+  void HandleStillState(bool event_handled);
+
+  /*
+   * Writes the buffer to the nunchuk.
+   */
+  bool WriteBuffer(const uint8_t *buffer, size_t length);
+
+  /*
+   * Reads data into the buffer from the nunchuk.
+   */
+  bool ReadBuffer(uint8_t *buffer, size_t length);
+
+  /*
+   * Sends an initialization sequence to the Nunchuk.
+   */
+  bool Initialize();
+};
+
+}  // namespace drivers
+
+#endif  // DRIVERS_NUNCHUK_H_
