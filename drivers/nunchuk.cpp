c8f006ea5d1276af318425c0613fb1c2c100cbf8
Andrew Rossignol <andrew.rossignol@gmail.com>
2016-08-05 23:26:31 -0700

    Trim down the Nunchuk driver.
    - Post events as they become available.

--- drivers/nunchuk.cpp
+++ drivers/nunchuk.cpp
@@ -15,13 +15,21 @@ float Nunchuk::NormalizeJoystickValue(int8_t value) {
   return static_cast<float>(value) / kMaxJoystickValue;
 }
 
+bool NunchukEventData::operator==(const NunchukEventData& event_data) const {
+  return event_data.joystick_x == joystick_x
+    && event_data.joystick_y == joystick_y
+    && event_data.accel_x == accel_x
+    && event_data.accel_y == accel_y
+    && event_data.accel_z == accel_z
+    && event_data.button_c == button_c
+    && event_data.button_z == button_z;
+}
+
 Nunchuk::Nunchuk(I2CDriver *i2c_driver, NunchukEventHandler *event_handler,
-                 int sample_period_ms, int max_still_count)
+                 int sample_period_ms)
     : i2c_driver_(i2c_driver),
       event_handler_(event_handler),
-      sample_period_ms_(sample_period_ms),
-      still_count_(0),
-      max_still_count_(max_still_count) {
+      sample_period_ms_(sample_period_ms) {
 }
 
 /*
@@ -51,88 +59,25 @@ void Nunchuk::Start() {
       if (!ReadBuffer(response, sizeof(response))) {
         break;
       }
-  
-      // Parse the buffer into the various parameters of the nunchuk.
-      int8_t joystick_x = (response[0] + 128);
-      int8_t joystick_y = (response[1] + 128);
-      int16_t accel_x = ((response[2] << 2) | ((response[5] >> 2) & 0x03));
-      int16_t accel_y = ((response[3] << 2) | ((response[5] >> 5) & 0x03));
-      int16_t accel_z = ((response[4] << 2) | ((response[5] >> 6) & 0x03));
-      ButtonState button_c = ParseButtonState(response[5], 1);
-      ButtonState button_z = ParseButtonState(response[5], 0);
-  
-      // Handle and post events.
-      bool event_handled = HandleAccelSample(accel_x, accel_y, accel_z);
-      event_handled |= HandleJoystickSample(joystick_x, joystick_y);
-      event_handled |= HandleButtonCSample(button_c);
-      event_handled |= HandleButtonZSample(button_z);
-      HandleStillState(event_handled);
+
+      NunchukEventData event_data = {
+        .joystick_x = static_cast<int8_t>((response[0] + 128)),
+        .joystick_y = static_cast<int8_t>((response[1] + 128)),
+        .accel_x = static_cast<int16_t>(((response[2] << 2)
+              | ((response[5] >> 2) & 0x03)) - 512),
+        .accel_y = static_cast<int16_t>(((response[3] << 2)
+              | ((response[5] >> 5) & 0x03)) - 512),
+        .accel_z = static_cast<int16_t>(((response[4] << 2)
+              | ((response[5] >> 6) & 0x03)) - 512),
+        .button_c = ParseButtonState(response[5], 1),
+        .button_z = ParseButtonState(response[5], 0),
+      };
+
+      // Post the event.
+      event_handler_->OnNunchukEvent(this, event_data);
   
       // Sleep for the sampling interval.
       chThdSleepMilliseconds(sample_period_ms_);
-    }
-  }
-}
-
-bool Nunchuk::HandleAccelSample(int16_t accel_x, int16_t accel_y,
-                                int16_t accel_z) {
-  if (accel_x != accel_x_ || accel_y != accel_y_ || accel_z != accel_z_) {
-    event_handler_->OnNunchukAccelChange(this, accel_x, accel_y, accel_z);
-    accel_x_ = accel_x;
-    accel_y_ = accel_y;
-    accel_z_ = accel_z;
-    return true;
-  }
-
-  return false;
-}
-
-bool Nunchuk::HandleButtonCSample(ButtonState button_c) {
-  if (button_c != button_c_) {
-    event_handler_->OnNunchukButtonCChange(this, button_c);
-    button_c_ = button_c;
-    return true;
-  }
-
-  return false;
-}
-
-bool Nunchuk::HandleButtonZSample(ButtonState button_z) {
-  if (button_z != button_z_) {
-    event_handler_->OnNunchukButtonZChange(this, button_z);
-    button_z_ = button_z;
-    return true;
-  }
-
-  return false;
-}
-
-bool Nunchuk::HandleJoystickSample(int8_t joystick_x, int8_t joystick_y) {
-  if (joystick_x != joystick_x_ || joystick_y != joystick_y_) {
-    event_handler_->OnNunchukJoystickChange(this, joystick_x, joystick_y);
-    joystick_x_ = joystick_x;
-    joystick_y_ = joystick_y;
-    return true;
-  }
-
-  return false;
-}
-
-void Nunchuk::HandleStillState(bool event_handled) {
-  if (event_handled) {
-    if (still_count_ == max_still_count_) {
-      event_handler_->OnNunchukInMotion(this);
-    }
-
-    still_count_ = 0;
-  } else {
-    if (still_count_ == (max_still_count_ - 1)) {
-      event_handler_->OnNunchukStill(this);
-    }
-
-    still_count_++;
-    if (still_count_ >= max_still_count_) {
-      still_count_ = max_still_count_;
     }
   }
 }
--- drivers/nunchuk.h
+++ drivers/nunchuk.h
@@ -36,6 +36,21 @@ enum class ButtonState : uint8_t {
 };
 
 /*
+ * The data associated with a Nunchuk event.
+ */
+struct NunchukEventData {
+  int8_t joystick_x;
+  int8_t joystick_y;
+  int16_t accel_x;
+  int16_t accel_y;
+  int16_t accel_z;
+  ButtonState button_c;
+  ButtonState button_z;
+
+  bool operator==(const NunchukEventData& event_data) const;
+};
+
+/*
  * An interface to handle Nunchuk events.
  */
 class NunchukEventHandler {
@@ -43,38 +58,10 @@ class NunchukEventHandler {
   ~NunchukEventHandler() {}
 
   /*
-   * Invoked when the joystick value has changed.
+   * Invoked when event data has been read from the Nunchuk.
    */
-  virtual void OnNunchukJoystickChange(Nunchuk *nunchuk, int8_t x, int8_t y) {}
-
-  /*
-   * Invoked when a 'C' button state change occurs.
-   */
-  virtual void OnNunchukButtonCChange(Nunchuk *nunchuk, ButtonState state) {}
-
-  /*
-   * Invoked when a 'Z' button state change occurs.
-   */
-  virtual void OnNunchukButtonZChange(Nunchuk *nunchuk, ButtonState state) {}
-
-  /*
-   * Invoked when the accelerometer value has changed.
-   */
-  virtual void OnNunchukAccelChange(Nunchuk* nunchuk,
-                                    int16_t x, int16_t y, int16_t z) {}
-
-  /*
-   * Invoked when the remote transitions from still to in motion. This happens
-   * when an event is published after the maximum number of no-event samples
-   * have been taken.
-   */
-  virtual void OnNunchukInMotion(Nunchuk *nunchuk) {}
-
-  /*
-   * Invoked when no events have been published for a maximum number of
-   * samples. The remote is assumed to be stationary if this happens.
-   */
-  virtual void OnNunchukStill(Nunchuk *nunchuk) {}
+  virtual void OnNunchukEvent(Nunchuk *nunchuk,
+                              const NunchukEventData& event_data) = 0;
 };
 
 /*
@@ -94,7 +81,7 @@ class Nunchuk : public util::NonCopyable {
    * This can be useful for detecting remote connection loss.
    */
   Nunchuk(I2CDriver *i2c_driver, NunchukEventHandler *event_handler,
-          int sample_period_ms, int max_stil_count);
+          int sample_period_ms);
 
   /*
    * Starts an event loop. Changes in controller state are exposed to a client
@@ -111,48 +98,6 @@ class Nunchuk : public util::NonCopyable {
 
   // The sampling period of data from the remote.
   int sample_period_ms_;
-
-  // Stillness detector state.
-  int still_count_;
-  const int max_still_count_;
-
-  // Current state of the joystick for event generation logic.
-  int8_t joystick_x_;
-  int8_t joystick_y_;
-  int16_t accel_x_;
-  int16_t accel_y_;
-  int16_t accel_z_;
-  ButtonState button_c_;
-  ButtonState button_z_;
-
-  /*
-   * Processes an accel sample and posts an event if necessary. Returns true
-   * if an event was generated.
-   */
-  bool HandleAccelSample(int16_t accel_x, int16_t accel_y, int16_t accel_z);
-
-  /*
-   * Processes a C button sample and posts an event if necessary. Returns true
-   * if an event was generated.
-   */
-  bool HandleButtonCSample(ButtonState button_c);
-
-  /*
-   * Processes a Z button sample and posts an event if necessary. Returns true
-   * if an event was generated.
-   */
-  bool HandleButtonZSample(ButtonState button_z);
-
-  /*
-   * Processes a joystick sample and posts an event if necessary. Returns true
-   * if an event was generated.
-   */
-  bool HandleJoystickSample(int8_t joystick_x, int8_t joystick_y);
-
-  /*
-   * Checks for no motion/any motion and posts an event as necessary.
-   */
-  void HandleStillState(bool event_handled);
 
   /*
    * Writes the buffer to the nunchuk.
