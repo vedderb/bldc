a01651c0859e1ab40e426d1381bf30cb949e15d6
Andrew Rossignol <andrew.rossignol@gmail.com>
2016-08-21 12:54:52 -0700

    Tune the minimum brake lights threshold to 0.0f.
    This was determined through field testing.

--- apps/nunchuk/nunchuk_manager.cpp
+++ apps/nunchuk/nunchuk_manager.cpp
@@ -16,7 +16,7 @@ using lighting::TurnSignalState;
 constexpr int kSamplingPeriodMs(10);
 constexpr int kMaxStillEventCount(50);
 constexpr float kDeadbandFilterThreshold(0.1f);
-constexpr float kBrakeThreshold(0.2f);
+constexpr float kBrakeThreshold(0.0f);
 constexpr float kSignalThreshold(0.9f);
 
 NunchukManager::NunchukManager(I2CDriver *i2c_driver,
