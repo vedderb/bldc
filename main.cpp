91f3534bdfea14941a8f4094ca2383868791ad99
Andrew Rossignol <andrew.rossignol@gmail.com>
2016-08-21 13:17:35 -0700

    Retab main.cpp to 2 space indentation.
    - Cleans up the copyright notice
    - Uses spaces for alignment in the DMA table
    - Adds my name to the copyright notice

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 /*
-	Copyright 2012-2015 Benjamin Vedder	benjamin@vedder.se
-
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    */
+ * Copyright 2012-2015 Benjamin Vedder benjamin@vedder.se
+ * Copyright 2016      Andrew Rossignol andrew.rossignol@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "ch.h"
 #include "hal.h"
@@ -65,16 +66,16 @@ using util::Singleton;
  * TIM3: servo_dec/Encoder (HW_R2)/servo_simple
  * TIM4: WS2811/WS2812 LEDs/Encoder (other HW)
  *
- * DMA/stream	Device		Function
- * 1, 2			I2C1		Nunchuk, temp on rev 4.5
- * 1, 7			I2C1		Nunchuk, temp on rev 4.5
- * 1, 1			UART3		HW_R2
- * 1, 3			UART3		HW_R2
- * 2, 2			UART6		Other HW
- * 2, 7			UART6		Other HW
- * 2, 4			ADC			mcpwm
- * 1, 0			TIM4		WS2811/WS2812 LEDs CH1 (Ch 1)
- * 1, 3			TIM4		WS2811/WS2812 LEDs CH2 (Ch 2)
+ * DMA/stream Device    Function
+ * 1, 2       I2C1      Nunchuk, temp on rev 4.5
+ * 1, 7       I2C1      Nunchuk, temp on rev 4.5
+ * 1, 1       UART3     HW_R2
+ * 1, 3       UART3     HW_R2
+ * 2, 2       UART6     Other HW
+ * 2, 7       UART6     Other HW
+ * 2, 4       ADC       mcpwm
+ * 1, 0       TIM4      WS2811/WS2812 LEDs CH1 (Ch 1)
+ * 1, 3       TIM4      WS2811/WS2812 LEDs CH2 (Ch 2)
  */
 
 static THD_WORKING_AREA(periodic_thread_wa, 1024);
@@ -85,120 +86,120 @@ static THD_WORKING_AREA(speed_controller_thread_wa, 2048);
 static THD_WORKING_AREA(nunchuk_manager_thread_wa, 2048);
 
 static THD_FUNCTION(periodic_thread, arg) {
-	(void)arg;
+  (void)arg;
 
-	chRegSetThreadName("Main periodic");
+  chRegSetThreadName("Main periodic");
 
-	for(;;) {
-		if (mc_interface_get_state() == MC_STATE_RUNNING) {
-			ledpwm_set_intensity(LED_GREEN, 1.0);
-		} else {
-			ledpwm_set_intensity(LED_GREEN, 0.2);
-		}
+  for(;;) {
+    if (mc_interface_get_state() == MC_STATE_RUNNING) {
+      ledpwm_set_intensity(LED_GREEN, 1.0);
+    } else {
+      ledpwm_set_intensity(LED_GREEN, 0.2);
+    }
 
-		mc_fault_code fault = mc_interface_get_fault();
-		if (fault != FAULT_CODE_NONE) {
-			for (int i = 0;i < (int)fault;i++) {
-				ledpwm_set_intensity(LED_RED, 1.0);
-				chThdSleepMilliseconds(250);
-				ledpwm_set_intensity(LED_RED, 0.0);
-				chThdSleepMilliseconds(250);
-			}
+    mc_fault_code fault = mc_interface_get_fault();
+    if (fault != FAULT_CODE_NONE) {
+      for (int i = 0;i < (int)fault;i++) {
+        ledpwm_set_intensity(LED_RED, 1.0);
+        chThdSleepMilliseconds(250);
+        ledpwm_set_intensity(LED_RED, 0.0);
+        chThdSleepMilliseconds(250);
+      }
 
-			chThdSleepMilliseconds(500);
-		} else {
-			ledpwm_set_intensity(LED_RED, 0.0);
-		}
+      chThdSleepMilliseconds(500);
+    } else {
+      ledpwm_set_intensity(LED_RED, 0.0);
+    }
 
-		if (mc_interface_get_state() == MC_STATE_DETECTING) {
-			commands_send_rotor_pos(mcpwm_get_detect_pos());
-		}
+    if (mc_interface_get_state() == MC_STATE_DETECTING) {
+      commands_send_rotor_pos(mcpwm_get_detect_pos());
+    }
 
-		disp_pos_mode display_mode = commands_get_disp_pos_mode();
+    disp_pos_mode display_mode = commands_get_disp_pos_mode();
 
-		switch (display_mode) {
-			case DISP_POS_MODE_ENCODER:
-				commands_send_rotor_pos(encoder_read_deg());
-				break;
+    switch (display_mode) {
+      case DISP_POS_MODE_ENCODER:
+        commands_send_rotor_pos(encoder_read_deg());
+        break;
 
-			case DISP_POS_MODE_PID_POS:
-				commands_send_rotor_pos(mc_interface_get_pid_pos_now());
-				break;
+      case DISP_POS_MODE_PID_POS:
+        commands_send_rotor_pos(mc_interface_get_pid_pos_now());
+        break;
 
-			case DISP_POS_MODE_PID_POS_ERROR:
-				commands_send_rotor_pos(utils_angle_difference(mc_interface_get_pid_pos_set(), mc_interface_get_pid_pos_now()));
-				break;
+      case DISP_POS_MODE_PID_POS_ERROR:
+        commands_send_rotor_pos(utils_angle_difference(mc_interface_get_pid_pos_set(), mc_interface_get_pid_pos_now()));
+        break;
 
-			default:
-				break;
-		}
+      default:
+        break;
+    }
 
-		if (mc_interface_get_configuration()->motor_type == MOTOR_TYPE_FOC) {
-			switch (display_mode) {
-			case DISP_POS_MODE_OBSERVER:
-				commands_send_rotor_pos(mcpwm_foc_get_phase_observer());
-				break;
+    if (mc_interface_get_configuration()->motor_type == MOTOR_TYPE_FOC) {
+      switch (display_mode) {
+      case DISP_POS_MODE_OBSERVER:
+        commands_send_rotor_pos(mcpwm_foc_get_phase_observer());
+        break;
 
-			case DISP_POS_MODE_ENCODER_OBSERVER_ERROR:
-				commands_send_rotor_pos(utils_angle_difference(mcpwm_foc_get_phase_observer(), mcpwm_foc_get_phase_encoder()));
-				break;
+      case DISP_POS_MODE_ENCODER_OBSERVER_ERROR:
+        commands_send_rotor_pos(utils_angle_difference(mcpwm_foc_get_phase_observer(), mcpwm_foc_get_phase_encoder()));
+        break;
 
-			default:
-				break;
-		}
-		}
+      default:
+        break;
+    }
+    }
 
-		chThdSleepMilliseconds(10);
-	}
+    chThdSleepMilliseconds(10);
+  }
 }
 
 static THD_FUNCTION(timer_thread, arg) {
-	(void)arg;
+  (void)arg;
 
-	chRegSetThreadName("msec_timer");
+  chRegSetThreadName("msec_timer");
 
-	for(;;) {
-		packet_timerfunc();
-		chThdSleepMilliseconds(1);
-	}
+  for(;;) {
+    packet_timerfunc();
+    chThdSleepMilliseconds(1);
+  }
 }
 
 int main(void) {
   // Initialize the HAL and ChibiOS.
-	halInit();
-	chSysInit();
+  halInit();
+  chSysInit();
 
-	hw_init_gpio();
+  hw_init_gpio();
 
-	LED_RED_OFF();
-	LED_GREEN_OFF();
+  LED_RED_OFF();
+  LED_GREEN_OFF();
 
-	conf_general_init();
-	ledpwm_init();
+  conf_general_init();
+  ledpwm_init();
 
-	mc_configuration mcconf;
-	conf_general_read_mc_configuration(&mcconf);
-	mc_interface_init(&mcconf);
+  mc_configuration mcconf;
+  conf_general_read_mc_configuration(&mcconf);
+  mc_interface_init(&mcconf);
 
-	commands_init();
-	comm_usb_init();
+  commands_init();
+  comm_usb_init();
 
-	app_configuration appconf;
-	conf_general_read_app_configuration(&appconf);
-	app_init(&appconf);
+  app_configuration appconf;
+  conf_general_read_app_configuration(&appconf);
+  app_init(&appconf);
 
-	timeout_init();
-	timeout_configure(appconf.timeout_msec, appconf.timeout_brake_current);
+  timeout_init();
+  timeout_configure(appconf.timeout_msec, appconf.timeout_brake_current);
 
 #if CAN_ENABLE
-	comm_can_init();
+  comm_can_init();
 #endif
 
 #if SERVO_OUT_ENABLE
 #if SERVO_OUT_SIMPLE
-	servo_simple_init();
+  servo_simple_init();
 #else
-	servo_init();
+  servo_init();
 #endif
 #endif
 
@@ -223,9 +224,9 @@ int main(void) {
       nunchuk_manager_thread_wa, sizeof(nunchuk_manager_thread_wa),
       NORMALPRIO, NunchukManagerThreadFunction, NULL); 
 
-	// Threads
-	chThdCreateStatic(periodic_thread_wa, sizeof(periodic_thread_wa), NORMALPRIO, periodic_thread, NULL);
-	chThdCreateStatic(timer_thread_wa, sizeof(timer_thread_wa), NORMALPRIO, timer_thread, NULL);
+  // Threads
+  chThdCreateStatic(periodic_thread_wa, sizeof(periodic_thread_wa), NORMALPRIO, periodic_thread, NULL);
+  chThdCreateStatic(timer_thread_wa, sizeof(timer_thread_wa), NORMALPRIO, timer_thread, NULL);
 
   // Suspend the main thread indefinitely.
   chThdSleepMilliseconds(TIME_INFINITE);
