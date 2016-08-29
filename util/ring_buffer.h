c7832403a95daa7222bb5521e0bda7278b290c3e
Andrew Rossignol <andrew.rossignol@gmail.com>
2016-08-28 22:55:50 -0700

    Adds a RingBuffer template.

---
+++ util/ring_buffer.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 Andrew Rossignol andrew.rossignol@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTIL_RING_BUFFER_H_
+#define UTIL_RING_BUFFER_H_
+
+#include "util/noncopyable.h"
+
+namespace util {
+
+template<typename T, size_t capacity_>
+class RingBuffer : public util::NonCopyable {
+ public:
+  /*
+   * Construct a RingBuffer.
+   */
+  RingBuffer();
+
+  /*
+   * Enqueues an element into the RingBuffer. If the buffer is full, it
+   * overwrites the oldest element.
+   *
+   * If it is not desirable to overwrite the oldest element, query the
+   * IsFull method before pushing.
+   */ 
+  void Enqueue(const T& element);
+
+  /*
+   * Dequeues an element from the RingBuffer. If the buffer is empty, it
+   * returns false.
+   */
+  bool Dequeue(T *element);
+
+  /*
+   * Returns a bool indicating whether or not the RingBuffer is full.
+   */
+  bool IsFull() const;
+
+  /*
+   * Returns a bool indicating whether or not the RingBuffer is empty. 
+   */
+  bool IsEmpty() const;
+
+  /*
+   * Returns the maximum number of elements that can be buffered by the
+   * ring buffer.
+   */
+  size_t capacity() const;
+
+  /*
+   * Returns the current number of elements in the ring buffer.
+   */
+  size_t size() const;
+
+ private:
+  T buffer_[capacity_];
+  size_t read_index_;
+  size_t write_index_;
+  size_t size_;
+};
+
+}  // namespace util
+
+#include "util/ring_buffer_impl.h"
+
+#endif  // UTIL_RING_BUFFER_H_
