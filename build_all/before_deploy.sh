189ba4f9bbeba5b758f98a4afd389f7794de101a
Ferdia McKeogh <chocol4te@users.noreply.github.com>
2018-04-24 20:32:40 +0100

    Fixed unexpected EOF in before_deploy.sh

--- build_all/before_deploy.sh
+++ build_all/before_deploy.sh
@@ -4,6 +4,6 @@ mkdir deploy
 
 for f in `find . -name '*bin'`
 do
-   filename=`echo $f | sed 's/\.\///; s/\/VESC//;'
+   filename=`echo $f | sed 's/\.\///; s/\/VESC//;'`
    cp $f deploy/$filename
 done
