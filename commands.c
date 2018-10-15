8acb60a4bb56dd6566cf37e68f9d8fe296cd84df
AlexanderG <alexandergrau@gmx.de>
2018-10-15 21:48:45 +0200

    float to int

--- commands.c
+++ commands.c
@@ -229,7 +229,7 @@ void commands_process_packet(unsigned char *data, unsigned int len) {
 	case COMM_SET_POS_CUMULATIVE:
 		ind = 0;
     float pos = ((float)buffer_get_int32(data, &ind) / 100000.0); // get position		    
-    float rpm = buffer_get_float32_auto(data, &ind); // get rpm parameter 
+    float rpm = ((float)buffer_get_int32(data, &ind)); // get rpm parameter 
     //commands_printf("pos=%.0f, rpm=%.0f", pos, rpm);
     /*if (rpm > 0.1){
       mcconf = *mc_interface_get_configuration(); // read current config
