93ebffa2ee19259acd125d13ae8e9042a473e86a
Marcos Chaparro <mchaparro@powerdesigns.ca>
2019-02-19 10:59:38 -0300

    Avoid watchdog resets if CAN is not used
    Signed-off-by: Marcos Chaparro <mchaparro@powerdesigns.ca>

--- timeout.c
+++ timeout.c
@@ -195,8 +195,10 @@ static THD_FUNCTION(timeout_thread, arg) {
 		// be monitored
 		if(feed_counter[THREAD_MCPWM] < MIN_THREAD_ITERATIONS)
 			threads_ok = false;
+#if CAN_ENABLE
 		if(feed_counter[THREAD_CANBUS] < MIN_THREAD_ITERATIONS)
 			threads_ok = false;
+#endif
 		if(feed_counter[THREAD_TIMER] < MIN_THREAD_ITERATIONS)
 			threads_ok = false;
 
