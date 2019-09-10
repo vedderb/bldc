5d3f829e69a9dac133ce9df066456444894b1ff8
Mitch Lustig <moesk8er2@gmail.com>
2019-09-10 01:38:03 -0700

    Fix conf gen

--- confgenerator.h
+++ confgenerator.h
@@ -9,7 +9,7 @@
 
 // Constants
 #define MCCONF_SIGNATURE		503309878
-#define APPCONF_SIGNATURE		0
+#define APPCONF_SIGNATURE		4290437352
 
 // Functions
 int32_t confgenerator_serialize_mcconf(uint8_t *buffer, const mc_configuration *conf);
