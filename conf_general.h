a3fcc27b8f4dbc8467bede46898abb0a8e2c6aff
CTSchorsch <g.schardt@fz-juelich.de>
2019-12-06 09:43:08 +0100

    define raus

--- conf_general.h
+++ conf_general.h
@@ -95,7 +95,7 @@
 //#define HW75_300_VEDDER_FIRST_PCB
 
 // Second revision with separate UART for NRF51
-#define HW75_300_REV_2
+//#define HW75_300_REV_2
 
 //#define HW_SOURCE "hw_75_300.c"
 //#define HW_HEADER "hw_75_300.h"
