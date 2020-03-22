d4f3986cf50caae6bcec2db2a1fda1c1302778fa
Benjamin Vedder <benjamin@vedder.se>
2020-03-22 13:50:32 +0100

    Removed old chibios, recompiled FWs, updated changelog

--- ChibiOS_3.0.2/os/various/cpp_wrappers/chcpp.mk
+++
@@ -1,5 +0,0 @@
-# C++ wrapper files.
-CHCPPSRC = ${CHIBIOS}/os/various/cpp_wrappers/ch.cpp \
-           ${CHIBIOS}/os/various/cpp_wrappers/syscalls_cpp.cpp
-
-CHCPPINC = ${CHIBIOS}/os/various/cpp_wrappers
