b2bab06172e2372bd43060fbd6a55224acac6664
Benjamin Vedder <benjamin@vedder.se>
2020-04-27 10:04:44 +0200

    Turned off test build flag

--- conf_general.h
+++ conf_general.h
@@ -24,7 +24,7 @@
 #define FW_VERSION_MAJOR			5
 #define FW_VERSION_MINOR			00
 // Set to 0 for building a release and iterate during beta test builds
-#define FW_TEST_VERSION_NUMBER		9
+#define FW_TEST_VERSION_NUMBER		0
 
 #include "datatypes.h"
 
