6196805d8c3a7c616b6582e8efa8c99ae068351e
Benjamin Vedder <benjamin@vedder.se>
2020-05-03 22:35:37 +0200

    Added CHUK_CTRL_TYPE_CURRENT_BIDIRECTIONAL, some fixes on last PR

--- conf_general.h
+++ conf_general.h
@@ -71,7 +71,7 @@
 
 // Mark3 version of HW60 with power switch and separate NRF UART.
 //#define HW60_IS_MK3
-#define HW60_IS_MK4
+//#define HW60_IS_MK4
 
 #define HW_SOURCE "hw_60.c"
 #define HW_HEADER "hw_60.h"
