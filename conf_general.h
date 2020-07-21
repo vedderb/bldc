5a52405a78165a901a59a3f9996706a7b83eb3ef
Frank Severinsen <FS@uavcomp.com>
2020-07-21 10:55:31 +0200

    include default HW

--- conf_general.h
+++ conf_general.h
@@ -140,8 +140,8 @@
 //#define HW_SOURCE "hw_unity.c"
 //#define HW_HEADER "hw_unity.h"
 
-#define HW_SOURCE "hw_uxv_sr.c"
-#define HW_HEADER "hw_uxv_sr.h"
+//#define HW_SOURCE "hw_uxv_sr.c"
+//#define HW_HEADER "hw_uxv_sr.h"
 
 //#define HW_DUAL_CONFIG_PARALLEL
 //#define HW_SOURCE "hw_stormcore_100d.c"
