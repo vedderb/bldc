6982b0e4726fb734930dbadafcd63529bd91fc1c
Jeffrey M. Friesen <jfriesen222@gmail.com>
2020-11-27 21:57:51 -0800

    Update conf_general.h

--- conf_general.h
+++ conf_general.h
@@ -134,15 +134,15 @@
 //#define HW_SOURCE "hw_100_250.c"
 //#define HW_HEADER "hw_100_250.h"
 
-#define HW_SOURCE "hw_unity.c"
-#define HW_HEADER "hw_unity.h"
+//#define HW_SOURCE "hw_unity.c"
+//#define HW_HEADER "hw_unity.h"
 
-#define HW_DUAL_CONFIG_PARALLEL
+//#define HW_DUAL_CONFIG_PARALLEL
 //#define HW_SOURCE "hw_stormcore_100d.c"
 //#define HW_HEADER "hw_stormcore_100d.h"
 
-//#define HW_SOURCE "hw_stormcore_60d.c"
-//#define HW_HEADER "hw_stormcore_60d.h"
+#define HW_SOURCE "hw_stormcore_60d.c"
+#define HW_HEADER "hw_stormcore_60d.h"
 //
 //#define HW_SOURCE "hw_stormcore_100s.c"
 //#define HW_HEADER "hw_stormcore_100s.h"
