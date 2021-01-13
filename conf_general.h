21c05bb14eaa15b42f6ffa01ecda7f4608fee7ee
CTSchorsch <g.schardt@fz-juelich.de>
2021-01-13 08:20:43 +0100

    fix conflicts

--- conf_general.h
+++ conf_general.h
@@ -80,8 +80,8 @@
 //#define HW60_IS_MK4
 //#define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
