2fe6c1344f97e1145f040540ec107433d6c4f14a
CTSchorsch <g.schardt@fz-juelich.de>
2021-04-04 09:28:20 +0200

    - change filenames, lora/lora.c as central file for all lora drivers - SX1278.x as first driver for e.g. RFM95W module

---
+++ lora/lora.h
@@ -0,0 +1,15 @@
+/*
+    */
+
+#ifndef HWCONF_RFM95W_H_
+#define HWCONF_RFM95W_H_
+
+#include "datatypes.h"
+#include "SX1278.h"
+#include "SX1278_hw.h"
+
+// Functions
+void rfm95w_init(void);
+void rfm95w_stop(void);
+
+#endif /* HWCONF_DRV8301_H_ */
--- lora/lora.mk
+++ lora/lora.mk
@@ -1,5 +1,5 @@
-LORASRC =   lora/SX1278.c \
-            lora/rfm95w.c \
-            lora/SX1278_hw.c 
+LORASRC = 	lora/SX1278.c \
+		lora/lora.c \
+		lora/SX1278_hw.c 
 
 LORAINC = 	lora
