dfc42defb1aac5129725de568e935e625bdc1cfc
Benjamin Vedder <benjamin@vedder.se>
2021-04-11 09:42:03 +0200

    Removed unneeded allocation and copy

--- commands.c
+++ commands.c
@@ -1166,14 +1166,12 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 	case COMM_GET_BATTERY_CUT: {
 		int32_t ind = 0;
 		uint8_t send_buffer[60];
-		mc_configuration *mcconf = mempools_alloc_mcconf();
-		*mcconf = *mc_interface_get_configuration();
+		volatile const mc_configuration *mcconf = mc_interface_get_configuration();
 
 		send_buffer[ind++] = packet_id;
 		buffer_append_float32(send_buffer, mcconf->l_battery_cut_start, 1e3, &ind);
 		buffer_append_float32(send_buffer, mcconf->l_battery_cut_end, 1e3, &ind);
 
-		mempools_free_mcconf(mcconf);
 		reply_func(send_buffer, ind);
 	} break;
 
