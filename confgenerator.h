187621d5302870fbcbcc531eba7608159a6eb8dd
Mitch Lustig <moesk8er2@gmail.com>
2021-04-17 18:20:51 -0700

    Add confgenerator

--- confgenerator.h
+++ confgenerator.h
@@ -9,7 +9,7 @@
 
 // Constants
 #define MCCONF_SIGNATURE		857427939
-#define APPCONF_SIGNATURE		3423413835
+#define APPCONF_SIGNATURE		2721948964
 
 // Functions
 int32_t confgenerator_serialize_mcconf(uint8_t *buffer, const mc_configuration *conf);
