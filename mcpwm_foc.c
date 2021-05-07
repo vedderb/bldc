3aaaeaf3a370958080bf98b93bb58b597b13c14a
Chas Leichner <chas@x.team>
2021-05-07 15:27:50 -0700

    Correct spelling, broken links, and missing parameters in documentation

--- mcpwm_foc.c
+++ mcpwm_foc.c
@@ -1399,6 +1399,10 @@ void mcpwm_foc_get_current_offsets(
  * @param current
  * The locking open loop current for the motor.
  *
+ * @param print
+ * Controls logging during the detection procedure. Set to true to enable
+ * logging.
+ *
  * @param offset
  * The detected offset.
  *
@@ -1407,6 +1411,10 @@ void mcpwm_foc_get_current_offsets(
  *
  * @param direction
  * The detected direction.
+ *
+ * @param inverted
+ * Is set to true if the encoder reports an increase in angle in the opposite
+ * direction of the motor.
  */
 void mcpwm_foc_encoder_detect(float current, bool print, float *offset, float *ratio, bool *inverted) {
 	mc_interface_lock();
@@ -1629,7 +1637,7 @@ void mcpwm_foc_encoder_detect(float current, bool print, float *offset, float *r
 }
 
 /**
- * Lock the motor with a current and sample the voiltage and current to
+ * Lock the motor with a current and sample the voltage and current to
  * calculate the motor resistance.
  *
  * @param current
