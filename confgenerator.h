3f1c7872f4e4092a493e5a12ee4fae5ffc25229c
Dado Mista <dadomista@gmail.com>
2021-08-15 16:22:59 -0700

    confgenerator for Balance: configurable speed/angle for each tiltback type
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- confgenerator.h
+++ confgenerator.h
@@ -9,7 +9,7 @@
 
 // Constants
 #define MCCONF_SIGNATURE		3706516163
-#define APPCONF_SIGNATURE		2302012416
+#define APPCONF_SIGNATURE		2855199595
 
 // Functions
 int32_t confgenerator_serialize_mcconf(uint8_t *buffer, const mc_configuration *conf);
