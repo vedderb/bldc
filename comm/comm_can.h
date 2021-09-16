9a23689456b09ca552608287e17b60f61c79370f
dr0ps <dr0ps@users.noreply.github.com>
2021-09-16 22:11:24 +0200

    Added uart functions

--- comm/comm_can.h
+++ comm/comm_can.h
@@ -42,6 +42,7 @@ void comm_can_set_current_off_delay(uint8_t controller_id, float current, float 
 void comm_can_set_current_brake(uint8_t controller_id, float current);
 void comm_can_set_rpm(uint8_t controller_id, float rpm);
 void comm_can_set_pos(uint8_t controller_id, float pos);
+void comm_can_set_handbrake(uint8_t controller_id, float current);
 void comm_can_set_current_rel(uint8_t controller_id, float current_rel);
 void comm_can_set_current_rel_off_delay(uint8_t controller_id, float current_rel, float off_delay);
 void comm_can_set_current_brake_rel(uint8_t controller_id, float current_rel);
