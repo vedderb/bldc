b0749cd06f155113ddb70937d9cd266ae41dba42
Kenn Sebesta <kenn@eissQ.com>
2021-12-02 07:05:43 -0500

    [Make][Python] Look for Python3

--- make/linux.mk
+++ make/linux.mk
@@ -5,8 +5,6 @@
 #   system.
 
 
-QT_SPEC ?= linux-g++
-
 ############################
 # Check for and find Python
 ############################
@@ -14,25 +12,28 @@ QT_SPEC ?= linux-g++
 
 # Get Python version, separate major/minor/patch, then put into wordlist
 PYTHON_VERSION_=$(wordlist 2,4,$(subst ., ,$(shell python -V 2>&1)))
+
 # Get major version from aforementioned list
 PYTHON_MAJOR_VERSION_=$(word 1,$(PYTHON_VERSION_))
+
 # Just in case Make has some weird scope stuff
 PYTHON=0
+
 # If the major Python version is the one we want..
-ifeq ($(PYTHON_MAJOR_VERSION_),2)
+ifeq ($(PYTHON_MAJOR_VERSION_),3)
 	# Then we can just use the normal Python executable
 	PYTHON:=python
 else
 	# However, this isn't always the case..
-	# Let's look for `python2`. If `which` doesn't return a null value, then
+	# Let's look for `python3`. If `which` doesn't return a null value, then
 	#  it exists!
-	ifneq ($(shell which python2), "")
-		PYTHON:=python2
+	ifneq ($(shell which python3), "")
+		PYTHON:=python3
 	else
 		# And if it doesn't exist, let's use the default Python, and warn the user.
 		# PYTHON NOT FOUND.
 		PYTHON:=python
-		echo "Python not found."
+		$(warning "Python3 not found.")
 	endif
 endif
 
