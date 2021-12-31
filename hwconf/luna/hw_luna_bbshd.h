a1dd7f4c7348f492bfe985f8b753653f65a4bf3c
Marcos Chaparro <mchaparro@powerdesigns.ca>
2021-12-31 10:52:47 -0300

    bbshd: include appconf
    Signed-off-by: Marcos Chaparro <mchaparro@powerdesigns.ca>

--- hwconf/luna/hw_luna_bbshd.h
+++ hwconf/luna/hw_luna_bbshd.h
@@ -23,7 +23,7 @@
 
 #define HW_NAME				"LUNA_BBSHD"
 #include "mcconf_luna_bbshd_52v.h"
-//#include "appconf_luna_bbshd.h" // Missing from pull request?
+#include "appconf_luna_bbshd.h"
 
 #define QMLUI_SOURCE_HW		"hwconf/luna/qmlui_luna_v1.c"
 #define QMLUI_HEADER_HW		"hwconf/luna/qmlui_luna_v1.h"
