53286b868da12471e0c965393aff193fea6f1a07
Benjamin Vedder <benjamin@vedder.se>
2022-01-16 00:46:20 +0100

    Updated bms print example

--- lispBM/tests/example_print_bms_data.lisp
+++ lispBM/tests/example_print_bms_data.lisp
@@ -23,10 +23,11 @@
 
 (let ((f (lambda ()
    (progn
+     (define v-cells (map (lambda (c)(get-bms-val "v_cell" c)) (range 0 (- (get-bms-val "cell_num") 1))))
      (print (list "V Tot  : " (get-bms-val "v_tot")))
-     (print (list "V Cells: " (map (lambda (c)(get-bms-val "v_cell" c)) (range 0 (- (get-bms-val "cell_num") 1)))))
+     (print (list "V Cells: " v-cells))
+     (print (list "foldl  : " (foldl + 0 v-cells)))
      (print (list "ADCs   : " (map (lambda (c)(get-adc c)) (iota 1))))
-     (print (list "foldl  : " (foldl + 0 (map (lambda (c)(get-bms-val "v_cell" c)) (range 0 (- (get-bms-val "cell_num") 1))))))
      (print " ")
      (yield (/ 1000000.0 rate))
      (f)
