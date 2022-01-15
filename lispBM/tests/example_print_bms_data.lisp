bf7bd9d8a56e4fafd602229a9f665bf75bfcffc2
Benjamin Vedder <benjamin@vedder.se>
2022-01-16 00:43:03 +0100

    Added foldl to example

--- lispBM/tests/example_print_bms_data.lisp
+++ lispBM/tests/example_print_bms_data.lisp
@@ -16,11 +16,17 @@
    (map (lambda (x) (+ x start)) (iota (- end start))
 )))
 
+(define foldl (lambda (f i xs)
+  (if (= xs nil)
+    i
+	(foldl f (f i (car xs)) (cdr xs)))))
+
 (let ((f (lambda ()
    (progn
      (print (list "V Tot  : " (get-bms-val "v_tot")))
      (print (list "V Cells: " (map (lambda (c)(get-bms-val "v_cell" c)) (range 0 (- (get-bms-val "cell_num") 1)))))
      (print (list "ADCs   : " (map (lambda (c)(get-adc c)) (iota 1))))
+     (print (list "foldl  : " (foldl + 0 (map (lambda (c)(get-bms-val "v_cell" c)) (range 0 (- (get-bms-val "cell_num") 1))))))
      (print " ")
      (yield (/ 1000000.0 rate))
      (f)
