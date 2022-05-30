d69b9943e996d5f18eb580953daeb19e79b58e36
JohnSpintend <liliqiou@qq.com>
2022-05-30 22:39:13 +0800

    JohnSpintend added Ubox files
    JohnSpintend added Ubox files

--- package_firmware.py
+++ package_firmware.py
@@ -91,6 +91,18 @@ package_dict["60_75"] = [['60_75', default_name],
                     ['60_75_no_limits', no_limits_name]]
 package_dict["60_75_mk2"] = [['60_75_mk2', default_name],
                     ['60_75_mk2_no_limits', no_limits_name]]
+package_dict["UBOX_SINGLE_75"] = [['ubox_single_75', default_name],
+                    ['ubox_single_75_no_limits', no_limits_name]]
+package_dict["UBOX_V1_75_MICRO"] = [['ubox_v1_75_micro', default_name],
+                    ['ubox_v1_75_micro_no_limits', no_limits_name]]
+package_dict["UBOX_V1_75_TYPEC"] = [['ubox_v1_75_typec', default_name],
+                    ['ubox_v1_75_typec_no_limits', no_limits_name]]
+package_dict["UBOX_V2_75"] = [['ubox_v2_75', default_name],
+                    ['ubox_v2_75_no_limits', no_limits_name]]
+package_dict["UBOX_SINGLE_100"] = [['ubox_single_100', default_name],
+                    ['ubox_single_100_no_limits', no_limits_name]]
+package_dict["UBOX_V2_100"] = [['ubox_v2_100', default_name],
+                    ['ubox_v2_100_no_limits', no_limits_name]]
 
 # This is the firmware stub string
 res_firmwares_string = '        <file>TARGET_DESTINATION_DIRECTORY/TARGET_DESTINATION_FILENAME</file>\n'
