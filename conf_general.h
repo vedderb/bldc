77906ba7815f2f146bfa2ad83299ebf7b63d1722
Benjamin Vedder <benjamin@vedder.se>
2023-01-24 11:16:08 +0100

    Updated version number

--- conf_general.h
+++ conf_general.h
@@ -24,7 +24,7 @@
 #define FW_VERSION_MAJOR			6
 #define FW_VERSION_MINOR			02
 // Set to 0 for building a release and iterate during beta test builds
-#define FW_TEST_VERSION_NUMBER		3
+#define FW_TEST_VERSION_NUMBER		4
 
 #include "datatypes.h"
 
