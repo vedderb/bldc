12415c4949803ce128fc908d0416e7ba705918a8
Teslafly <2079881+Teslafly@users.noreply.github.com>
2023-03-08 03:01:21 -0600

    reverse first

--- hwconf/trampa/75_300/hw_75_300_core.h
+++ hwconf/trampa/75_300/hw_75_300_core.h
@@ -184,10 +184,10 @@
 
 // Hardcoded adc app pins that won't override uart port or ppm inputs. On swd pins for example
 // #define HW_HARDWARE_ADC_APP_PINS
-// #define HW_CRUISE_SWITCH_PORT 			GPIOA
-// #define HW_CRUISE_SWITCH_PIN 			  14
 // #define HW_REVERSE_SWITCH_PORT	 		GPIOA
 // #define HW_REVERSE_SWITCH_PIN 			13
+// #define HW_CRUISE_SWITCH_PORT 			GPIOA
+// #define HW_CRUISE_SWITCH_PIN 			  14
 
 // UART Peripheral
 #define HW_UART_DEV				SD3
