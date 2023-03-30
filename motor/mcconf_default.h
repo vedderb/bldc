f061afa554610c68427c241f8271e9a86b52312c
Marcos Chaparro <mchaparro@powerdesigns.ca>
2023-03-30 09:28:00 -0300

    Add regen cutoff to prevent overcharging the battery during braking
    This change helps ensure that the battery remains in good condition, prolongs its overall lifespan and avoids hazardous scenarios of overcharge.
    A soft cutoff for the regenerative braking is implemented, in which start and end thresholds should be set in VESC Tool. The end threshold should be set such that the cells don't exceed 4.2v in a typical Li-Ion battery.
    
    Signed-off-by: Marcos Chaparro <marcos@lunacycle.com>

--- motor/mcconf_default.h
+++ motor/mcconf_default.h
@@ -62,6 +62,12 @@
 #ifndef MCCONF_L_BATTERY_CUT_END
 #define MCCONF_L_BATTERY_CUT_END		8.0		// Limit the positive current completely at this voltage
 #endif
+#ifndef MCCONF_L_BATTERY_REGEN_CUT_START
+#define MCCONF_L_BATTERY_REGEN_CUT_START    55.0	// Start limiting the regen current at this voltage
+#endif
+#ifndef MCCONF_L_BATTERY_REGEN_CUT_END
+#define MCCONF_L_BATTERY_REGEN_CUT_END  57.0		// Limit the regen current completely at this voltage
+#endif
 #ifndef MCCONF_L_RPM_MAX
 #define MCCONF_L_RPM_MAX				100000.0	// The motor speed limit (Upper)
 #endif
