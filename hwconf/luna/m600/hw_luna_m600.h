e5ee0fac766185a65a8d49ea9d0d0d8af00e406f
diego_palta <diego_palta@PaltaTech>
2023-08-22 12:23:54 -0300

    Luna M600: update appconf_luna_m600.h file. Add abs min and max ADC1 voltage

--- hwconf/luna/m600/hw_luna_m600.h
+++ hwconf/luna/m600/hw_luna_m600.h
@@ -21,7 +21,7 @@
 #ifndef HW_LUNA_M600_H_
 #define HW_LUNA_M600_H_
 
-#define FW_NAME					"2023.06.27"
+#define FW_NAME					"2023.08.22"
 
 #include "mcconf_luna_m600.h"
 #include "appconf_luna_m600.h"
