aa073f13df7161abb85c8fd4dab809e82c7cb586
Marcos Chaparro <mchaparro@powerdesigns.ca>
2023-12-06 13:23:25 -0300

    luna_m600: Fine tune motor parameters and use full clarke to avoid rpm runaway
    Signed-off-by: Marcos Chaparro <mchaparro@powerdesigns.ca>

--- hwconf/luna/m600/hw_luna_m600.h
+++ hwconf/luna/m600/hw_luna_m600.h
@@ -21,7 +21,7 @@
 #ifndef HW_LUNA_M600_H_
 #define HW_LUNA_M600_H_
 
-#define FW_NAME					"2023.08.22"
+#define FW_NAME					"2023.12.06"
 
 #include "mcconf_luna_m600.h"
 #include "appconf_luna_m600.h"
