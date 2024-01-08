a45b6a02228860dd287a7f38fdb7dd892b7c28d2
makerbase-motor <38064527+makerbase-motor@users.noreply.github.com>
2024-01-08 18:36:42 +0800

    Fix. h file error

--- hwconf/makerbase/75_200_V2/hw_mksesc_75_200_v2.h
+++ hwconf/makerbase/75_200_V2/hw_mksesc_75_200_v2.h
@@ -17,11 +17,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef HW_75_200_V2_H_
-#define HW_75_200_V2_H_
+#ifndef HW_MKSESC_75_200_V2_H_
+#define HW_MKSESC_75_200_V2_H_
 
 #define HW75_200_V2
 
 #include "hw_mksesc_75_200_v2_core.h"
 
-#endif /* HW_75_200_V2_H_ */
+#endif /* HW_MKSESC_75_200_V2_H_ */
