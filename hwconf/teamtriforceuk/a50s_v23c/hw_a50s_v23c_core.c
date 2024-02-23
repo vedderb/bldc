d41466f5a31f7d3e848d31a82144016b29efb8d3
Euan <euan.mutch@gmail.com>
2024-02-23 23:20:37 +0000

    Backport to A50S V23 which also has 25MHz clock

--- hwconf/teamtriforceuk/a50s_v23c/hw_a50s_v23c_core.c
+++ hwconf/teamtriforceuk/a50s_v23c/hw_a50s_v23c_core.c
@@ -144,10 +144,8 @@ void hw_setup_adc_channels(void) {
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 4, ADC_SampleTime_15Cycles);          // 9 -  ADC_IND_CURR2
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 5, ADC_SampleTime_15Cycles);          // 12 -  ADC_IND_CURR2
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 6, ADC_SampleTime_15Cycles);          // 15 -  ADC_IND_CURR2
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 7, ADC_SampleTime_15Cycles);          // 18 -  ADC_IND_CURR2
-	
-	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 8, ADC_SampleTime_15Cycles);	         // 21 -  ADC_IND_SENS3
-	
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 7, ADC_SampleTime_15Cycles);          // 18 -  ADC_IND_CURR2	
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 8, ADC_SampleTime_15Cycles);	         // 21 -  ADC_IND_SENS3	
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 9, ADC_SampleTime_15Cycles);           // 24 -  ADC_IND_EXT	
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_Vrefint, 10, ADC_SampleTime_56Cycles);    // 27 - ADC_IND_VREFINT     
 	
@@ -159,10 +157,8 @@ void hw_setup_adc_channels(void) {
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 4, ADC_SampleTime_15Cycles);          // 10 -  ADC_IND_CURR1
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 5, ADC_SampleTime_15Cycles);          // 13 -  ADC_IND_CURR1
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 6, ADC_SampleTime_15Cycles);          // 16 -  ADC_IND_CURR1
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 7, ADC_SampleTime_15Cycles);          // 19 -  ADC_IND_CURR1
-	
-	ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 8, ADC_SampleTime_15Cycles);	         // 22 -  ADC_IND_SENS2
-	
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_11, 7, ADC_SampleTime_15Cycles);          // 19 -  ADC_IND_CURR1	
+	ADC_RegularChannelConfig(ADC2, ADC_Channel_1, 8, ADC_SampleTime_15Cycles);	         // 22 -  ADC_IND_SENS2	
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_6, 9, ADC_SampleTime_15Cycles);           // 25 -  ADC_IND_EXT2
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_14, 10, ADC_SampleTime_56Cycles);     	 // 28 -  ADC_IND_TEMP_MOTOR
 		
@@ -174,10 +170,8 @@ void hw_setup_adc_channels(void) {
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 4, ADC_SampleTime_15Cycles);          // 11 -  UNUSED
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 5, ADC_SampleTime_15Cycles);          // 14 -  UNUSED
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 6, ADC_SampleTime_15Cycles);          // 17 -  UNUSED
-	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 7, ADC_SampleTime_15Cycles);          // 20 -  UNUSED
-	
-	ADC_RegularChannelConfig(ADC3, ADC_Channel_2, 8, ADC_SampleTime_15Cycles);	         // 23 -  ADC_IND_SENS1
-	
+	ADC_RegularChannelConfig(ADC3, ADC_Channel_13, 7, ADC_SampleTime_15Cycles);          // 20 -  UNUSED	
+	ADC_RegularChannelConfig(ADC3, ADC_Channel_2, 8, ADC_SampleTime_15Cycles);	         // 23 -  ADC_IND_SENS1	
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_3, 9, ADC_SampleTime_15Cycles);           // 26 -  ADC_IND_TEMP_MOS
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_15, 10, ADC_SampleTime_56Cycles);     	 // 29 - UNUSED
 
