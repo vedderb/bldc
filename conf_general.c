adfe4a736ea060a9fb462f82a719cb714d94a063
Benjamin Vedder <benjamin@vedder.se>
2024-03-03 13:17:06 +0100

    Measure flux linkage at 25 kHz when sampling in both zero vectors

--- conf_general.c
+++ conf_general.c
@@ -1818,7 +1818,11 @@ int conf_general_detect_apply_all_foc(float max_power_loss,
 #ifdef HW_HAS_DUAL_MOTORS
 	mcconf->foc_f_zv = 25000.0;
 #else
-	mcconf->foc_f_zv = 40000.0;
+	if (mcconf->foc_control_sample_mode == FOC_CONTROL_SAMPLE_MODE_V0_V7) {
+		mcconf->foc_f_zv = 25000.0;
+	} else {
+		mcconf->foc_f_zv = 40000.0;
+	}
 #endif
 	mc_interface_set_configuration(mcconf);
 
