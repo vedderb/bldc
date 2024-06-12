b7d9a72878addf0da27266adba5b9e75463d0421
Benjamin Vedder <benjamin@vedder.se>
2024-06-12 23:18:46 +0200

    Added hw_basic

--- package_firmware.py
+++ package_firmware.py
@@ -175,6 +175,8 @@ package_dict["RB"] = [['rb', default_name]]
 package_dict["STR365"] = [['str365', default_name],
                     ['str365_no_limits', no_limits_name]]
 package_dict["SPARKF"] = [['sparkf', default_name]]
+package_dict["VESC_BASIC"] = [['basic', default_name],
+                    ['basic_no_limits', no_limits_name]]
 
 # This is the firmware stub string
 res_firmwares_string = '        <file>TARGET_DESTINATION_DIRECTORY/TARGET_DESTINATION_FILENAME</file>\n'
