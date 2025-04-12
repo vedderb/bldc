84efdebe114a8411b9b5f7bd2098d2b1bd04199c
conradopoole <conrado.poole@gmail.com>
2025-04-12 13:20:47 -0700

    JetFleet F6 Variants

--- hwconf/JetFleet/hw_JetFleetF6_20s.h
+++ hwconf/JetFleet/hw_JetFleetF6_20s.h
@@ -17,11 +17,21 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     */
 
-#ifndef HW_JetFleetF6_H_
-#define HW_JetFleetF6_H_
+#ifndef HW_JetFleetF6_20s_H_
 
-#define HW_NAME					"JetFleetF6"
+#define HW_JetFleetF6_20s_H_
+
+#define HW_NAME					"JetFleetF6_20s"
+#define HW_JetFleetF6_20s
+
+#define VIN_R2				            4700.0
+#define MCCONF_L_MAX_VOLTAGE			90.0	// Maximum input voltage
+#define MCCONF_L_MAX_ABS_CURRENT		260.0	// The maximum absolute current above which a fault is generated
+#define HW_LIM_CURRENT			        -260.0, 260.0
+#define HW_LIM_CURRENT_ABS		        0.0, 320.0
+#define HW_LIM_VIN			            18.0, 90.0
+
 
 #include "hw_JetFleetF6_core.h"
 
-#endif
+#endif /* HW_JetFleetF6_20s_H_ */
