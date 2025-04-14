e425364a76a782f472cd100929eb7c9a1a7d2df3
Dado Mista <dadomista@gmail.com>
2025-04-14 09:25:48 -0700

    Fungineers Thor400v2 added
    Signed-off-by: Dado Mista <dadomista@gmail.com>

---
+++ hwconf/fungineers/400/hw_Thor400v2.h
@@ -0,0 +1,31 @@
+/*
+	Copyright 2022 Benjamin Vedder	benjamin@vedder.se
+
+	This file is part of the VESC firmware.
+
+	The VESC firmware is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The VESC firmware is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    */
+
+#ifndef HW_Thor400_H_
+#define HW_Thor400_H_
+
+#define HW_NAME					"Thor400v2"
+
+#define CURRENT_SHUNT_RES		(0.00025)
+#define CURRENT_AMP_GAIN		15.0
+
+
+#include "hw_Thor400_core.h"
+
+#endif
