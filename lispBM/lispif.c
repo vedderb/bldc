55590ececaecc8e00a36e00b6ffa826abf002ec9
Benjamin Vedder <benjamin@vedder.se>
2025-06-19 17:09:48 +0200

    Only save const heap ptr after stop if eval was running before

--- lispBM/lispif.c
+++ lispBM/lispif.c
@@ -704,9 +704,11 @@ bool lispif_restart(bool print, bool load_code, bool load_imports) {
 	if (!load_code || (code_data != 0 && code_len > 0)) {
 		lispif_disable_all_events();
 
+		bool save_heap = lisp_thd_running && lbm_image_exists();
+
 		lispif_stop();
 
-		if (lbm_image_exists()) {
+		if (save_heap) {
 			lbm_image_save_constant_heap_ix();
 		}
 
