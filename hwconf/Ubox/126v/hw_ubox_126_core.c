e276ce17bf23ab48abd12aa14d2bbf5809a5509d
JohnSpintend <98016110+JohnSpintend@users.noreply.github.com>
2025-08-26 21:24:39 +0800

    Commented an unused variable
    Commented an unused variable: uint8_t power_key_click

--- hwconf/Ubox/126v/hw_ubox_126_core.c
+++ hwconf/Ubox/126v/hw_ubox_126_core.c
@@ -1,5 +1,5 @@
 /*
-	Copyright 2018 Benjamin Vedder	benjamin@vedder.se
+	BCopyright 2018 Benjamin Vedder	benjamin@vedder.se
 
 	This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,8 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     */
-
-//Copy from "hw_75_300_core.c"
+//Copy from "hw_100_250.h"
 
 #include "hw.h"
 
@@ -101,13 +100,6 @@ void hw_init_gpio(void) {
 			PAL_MODE_OUTPUT_PUSHPULL |
 			PAL_STM32_OSPEED_HIGHEST);
 	PHASE_FILTER_OFF();
-
-		// Current filter
-	palSetPadMode(GPIOD, 2,
-			PAL_MODE_OUTPUT_PUSHPULL |
-			PAL_STM32_OSPEED_HIGHEST);
-
-	CURRENT_FILTER_OFF();
 
 	// AUX pin
 	AUX_OFF();
@@ -274,7 +266,7 @@ void hw_try_restore_i2c(void) {
 	}
 }
 
-float hw75_300_get_temp(void) {
+float hw100_250_get_temp(void) {
 	float t1 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
 	float t2 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_2]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
 	float t3 = (1.0 / ((logf(NTC_RES(ADC_Value[ADC_IND_TEMP_MOS_3]) / 10000.0) / 3380.0) + (1.0 / 298.15)) - 273.15);
@@ -353,7 +345,7 @@ static THD_FUNCTION(shutdown_thread, arg) {
 	uint64_t odometer_old = mc_interface_get_odometer();
 
 	for(;;)	{
-		//uint8_t power_key_click = 0;
+		uint8_t power_key_click = 0;
 		uint32_t sys_time_ms = chVTGetSystemTime() / (float)CH_CFG_ST_FREQUENCY * 1000.0f;
 
 		float dt = (float)chVTTimeElapsedSinceX(last_iteration_time) / (float)CH_CFG_ST_FREQUENCY;
@@ -376,7 +368,7 @@ static THD_FUNCTION(shutdown_thread, arg) {
 	        } else {
 	            if((power_key_pressed_ms > 50) && (power_key_pressed_ms < 500)) {
 					// TODO: Could toggle different led types here??
-	                //power_key_click = 1;
+	                power_key_click = 1;
 	            }
 	            power_key_pressed_ms = 0;
 	        }
